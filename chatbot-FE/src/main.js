6f66f935e8b0ea86a75ddc55026bf67a1c5916d1
Anže Oblak <anze.oblak@rettro.si>
2025-04-19 21:08:29 +0200

    Initial commit

---
+++ chatbot-FE/src/main.js
@@ -0,0 +1,25 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import Aura from "@primeuix/themes/aura";
+import PrimeVue from "primevue/config";
+import router from "./router";
+
+import "./styles/app.css";
+
+const app = createApp(App);
+
+app.use(router);
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      cssLayer: {
+        name: "primevue",
+        order: "theme, base, primevue",
+      },
+    },
+  },
+});
+
+app.mount("#app");
---
+++ chatbot-FE/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { PrimeVueResolver } from "@primevue/auto-import-resolver";
+import Components from "unplugin-vue-components/vite";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    tailwindcss(),
+    Components({
+      resolvers: [PrimeVueResolver()],
+    }),
+  ],
+});
